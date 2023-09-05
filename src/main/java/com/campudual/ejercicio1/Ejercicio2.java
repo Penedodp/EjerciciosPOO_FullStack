c84232d1f4392a9a1ec2e824148a2d695a94145e
David Penedo Cerqueira <fullstack_david@outlook.com>
2023-09-05 13:16:30 +0200

    Ver si es bisiesto

--- src/main/java/com/campudual/ejercicio1/Ejercicio2.java
+++ src/main/java/com/campudual/ejercicio1/Ejercicio2.java
@@ -1,10 +1,14 @@
 package com.campudual.ejercicio1;
 
-import java.time.Year;
+import java.util.Scanner;
 
 public class Ejercicio2 {
     public static void main(String[] args) {
-        int year = 2005;
+        Integer year ;
+        System.out.println("Escriba un año");
+        Scanner teclado = new Scanner(System.in);
+        year = teclado.nextInt();
+
 
         if (esBisiesto(year)) {
             System.out.println("El año es bisiesto");
